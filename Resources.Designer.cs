8d5784e0c0122c21d7189703346628ccc71fc728
strick-j <skazoo@gmail.com>
2022-07-27 14:40:38 -0400

    Updated Resources.Designer.cs

--- Resources.Designer.cs
+++ Resources.Designer.cs
@@ -59,5 +59,511 @@ namespace CyberArk.Extensions.Identity.AdmAccts {
                 resourceCulture = value;
             }
         }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Invalid client creds or client not allowed..
+        /// </summary>
+        internal static string AccessDenied
+        {
+            get
+            {
+                return ResourceManager.GetString("AccessDenied", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to response failure. .
+        /// </summary>
+        internal static string ActionResponseFailure
+        {
+            get
+            {
+                return ResourceManager.GetString("ActionResponseFailure", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to  response validated. JSON indicates action was successful. .
+        /// </summary>
+        internal static string ActionResponseSuccess
+        {
+            get
+            {
+                return ResourceManager.GetString("ActionResponseSuccess", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to 400 Bad Request Failure. Checking failure type..
+        /// </summary>
+        internal static string BadRequestRecieved
+        {
+            get
+            {
+                return ResourceManager.GetString("BadRequestRecieved", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Change Action Ended Successfully..
+        /// </summary>
+        internal static string ChangeSuccess
+        {
+            get
+            {
+                return ResourceManager.GetString("ChangeSuccess", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Timeout Error. Unable to resolve address or unable to connect to address..
+        /// </summary>
+        internal static string ErrorTimeout
+        {
+            get
+            {
+                return ResourceManager.GetString("ErrorTimeout", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Unknown error occured: .
+        /// </summary>
+        internal static string GenericError
+        {
+            get
+            {
+                return ResourceManager.GetString("GenericError", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Generic Token Request Exception:  .
+        /// </summary>
+        internal static string GenericTokenException
+        {
+            get
+            {
+                return ResourceManager.GetString("GenericTokenException", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to WebException: Connection Failure. Unable to connect to the remote server..
+        /// </summary>
+        internal static string HttpConnectFailure
+        {
+            get
+            {
+                return ResourceManager.GetString("HttpConnectFailure", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Http Request Exeption: Response status code does not indicate success: 400 (Bad Request). No Inner Exception available..
+        /// </summary>
+        internal static string HttpGeneralRequestError
+        {
+            get
+            {
+                return ResourceManager.GetString("HttpGeneralRequestError", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to WebException: Name Resolution Error. The remote name could not be resolved..
+        /// </summary>
+        internal static string HttpNameResolutionFailure
+        {
+            get
+            {
+                return ResourceManager.GetString("HttpNameResolutionFailure", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Non 200 Http Response Recieved. The Identity Web Server is not available or the user credentials were not accepted..
+        /// </summary>
+        internal static string HttpRequestException
+        {
+            get
+            {
+                return ResourceManager.GetString("HttpRequestException", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Http Request Exeption: The underlying connection was closed: Could not establish trust relationship for the SSL/TLS secure channel.
+        /// </summary>
+        internal static string HttpSecureChannelError
+        {
+            get
+            {
+                return ResourceManager.GetString("HttpSecureChannelError", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to InnerException: Unhandled..
+        /// </summary>
+        internal static string HttpUnhandledInnerException
+        {
+            get
+            {
+                return ResourceManager.GetString("HttpUnhandledInnerException", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to WebException: Unhandled..
+        /// </summary>
+        internal static string HttpUnhandledInnerWebException
+        {
+            get
+            {
+                return ResourceManager.GetString("HttpUnhandledInnerWebException", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Unknown Application..
+        /// </summary>
+        internal static string InvalidApp
+        {
+            get
+            {
+                return ResourceManager.GetString("InvalidApp", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Client Credentials Authentication Mode is not enabled for this App..
+        /// </summary>
+        internal static string InvalidAuthMode
+        {
+            get
+            {
+                return ResourceManager.GetString("InvalidAuthMode", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Invalid Client or Credentials..
+        /// </summary>
+        internal static string InvalidClientOrCreds
+        {
+            get
+            {
+                return ResourceManager.GetString("InvalidClientOrCreds", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Invalid Grant Type.
+        /// </summary>
+        internal static string InvalidGrantType
+        {
+            get
+            {
+                return ResourceManager.GetString("InvalidGrantType", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Unknown scope requested..
+        /// </summary>
+        internal static string InvalidScope
+        {
+            get
+            {
+                return ResourceManager.GetString("InvalidScope", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to User not able to access Identity Application.
+        /// </summary>
+        internal static string InvalidUserAccess
+        {
+            get
+            {
+                return ResourceManager.GetString("InvalidUserAccess", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Logon Action Ended Successfully..
+        /// </summary>
+        internal static string LogonSuccess
+        {
+            get
+            {
+                return ResourceManager.GetString("LogonSuccess", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Password update failed. Please make sure the password meet the complexity requirements. Password must be at least 8 characters long, not be longer than 64 characters, contain at least one digit, contain at least one upper and one lower case letter..
+        /// </summary>
+        internal static string PasswordComplexity
+        {
+            get
+            {
+                return ResourceManager.GetString("PasswordComplexity", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to After parsing a value an unexpected character was encountered. .
+        /// </summary>
+        internal static string PasswordJsonRead
+        {
+            get
+            {
+                return ResourceManager.GetString("PasswordJsonRead", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Password update failed. Please make sure the new password is not one of last few passwords..
+        /// </summary>
+        internal static string PasswordLastN
+        {
+            get
+            {
+                return ResourceManager.GetString("PasswordLastN", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Password update failed. Please make sure the new password is not the same as the current one..
+        /// </summary>
+        internal static string PasswordSameAsOld
+        {
+            get
+            {
+                return ResourceManager.GetString("PasswordSameAsOld", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Prereconcile Action Ended Successfully..
+        /// </summary>
+        internal static string PrereconcileSuccess
+        {
+            get
+            {
+                return ResourceManager.GetString("PrereconcileSuccess", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to  request response recieved. Validating JSON response success value..
+        /// </summary>
+        internal static string RecieveActionResponse
+        {
+            get
+            {
+                return ResourceManager.GetString("RecieveActionResponse", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Reconcile Action Ended Successfully..
+        /// </summary>
+        internal static string ReconcileSuccess
+        {
+            get
+            {
+                return ResourceManager.GetString("ReconcileSuccess", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Missing required parameter..
+        /// </summary>
+        internal static string RequiredParameter
+        {
+            get
+            {
+                return ResourceManager.GetString("RequiredParameter", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Sending request for .
+        /// </summary>
+        internal static string SendActionRequest
+        {
+            get
+            {
+                return ResourceManager.GetString("SendActionRequest", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to You are not authorized to perform this operation. Please contact your IT helpdesk..
+        /// </summary>
+        internal static string ServiceNotAuthorized
+        {
+            get
+            {
+                return ResourceManager.GetString("ServiceNotAuthorized", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Timeout Token Request Exception: .
+        /// </summary>
+        internal static string TimeoutTokenException
+        {
+            get
+            {
+                return ResourceManager.GetString("TimeoutTokenException", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Unable to extract bearer token from Identity Server response..
+        /// </summary>
+        internal static string TokenError
+        {
+            get
+            {
+                return ResourceManager.GetString("TokenError", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Extracted bearer token from Identity Server response..
+        /// </summary>
+        internal static string TokenSuccess
+        {
+            get
+            {
+                return ResourceManager.GetString("TokenSuccess", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Unahandled API Response recieved: .
+        /// </summary>
+        internal static string UnhandledApiResponse
+        {
+            get
+            {
+                return ResourceManager.GetString("UnhandledApiResponse", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Unhandled Status Code recieved: .
+        /// </summary>
+        internal static string UnhandledApiStatusCode
+        {
+            get
+            {
+                return ResourceManager.GetString("UnhandledApiStatusCode", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to WebException: Unhandled -  .
+        /// </summary>
+        internal static string UnhandledWebException
+        {
+            get
+            {
+                return ResourceManager.GetString("UnhandledWebException", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Updating HttpClient authentication header to utilize Oauth2 Bearer Token..
+        /// </summary>
+        internal static string UpdateAuthenticationHeader
+        {
+            get
+            {
+                return ResourceManager.GetString("UpdateAuthenticationHeader", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to User Id not found..
+        /// </summary>
+        internal static string UserIdNotFound
+        {
+            get
+            {
+                return ResourceManager.GetString("UserIdNotFound", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to You are not authorized to perform this operation. Please contact your IT helpdesk..
+        /// </summary>
+        internal static string UserNotAuthorized
+        {
+            get
+            {
+                return ResourceManager.GetString("UserNotAuthorized", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to User not found; they may have been deleted or the directory may be unavailable..
+        /// </summary>
+        internal static string UserNotFound
+        {
+            get
+            {
+                return ResourceManager.GetString("UserNotFound", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Unable to extract UUID from Identity Server response..
+        /// </summary>
+        internal static string UuidError
+        {
+            get
+            {
+                return ResourceManager.GetString("UuidError", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Extracted UUID from Identity Server Resposne..
+        /// </summary>
+        internal static string UuidSuccess
+        {
+            get
+            {
+                return ResourceManager.GetString("UuidSuccess", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Validating JSON response success value..
+        /// </summary>
+        internal static string ValidatingResponse
+        {
+            get
+            {
+                return ResourceManager.GetString("ValidatingResponse", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Verify Action Ended Successfully..
+        /// </summary>
+        internal static string VerifySuccess
+        {
+            get
+            {
+                return ResourceManager.GetString("VerifySuccess", resourceCulture);
+            }
+        }
     }
 }
